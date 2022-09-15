9f943834dcb60eb1be5c27e3602e3d27ac668602
aliaa gheis <aliaagheis@gmail.com>
2022-09-15 22:16:07 +0200

    style:overview course section

--- src/components/Course/Overview.js
+++ src/components/Course/Overview.js
@@ -3,17 +3,19 @@ import { useContext } from 'react'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { SummaryContext } from '.'
+import styles from './styles/Overview.module.css'
+
 export default function Overview() {
 
     const { objectives_summary } = useContext(SummaryContext);
     return (
-        <section className='card' style={{ padding: '2rem', marginTop: '3rem' }} id='overview'>
+        <section className={`card ${styles.container}`} id='overview'>
             <h3>What you'll learn</h3>
             <Row xs={1} sm={2}>
                 {
                     objectives_summary.map((e, i) => (
 
-                        <Col key={i} as='li'>
+                        <Col key={i} as='li' className={styles.point}>
                             <span className="material-symbols-outlined">
                                 done
                             </span>
